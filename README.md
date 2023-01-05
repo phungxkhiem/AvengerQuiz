851b0a705fb6090927f53fabd64467b69c931ea8
Khiem Thanh Phung <khiemphung@khiems-mbp.attlocal.net>
2023-01-04 19:26:57 -0800

    finished question for avenger quiz

--- README.md
+++
@@ -1,2 +0,0 @@
-# AvengerQuiz
-AvengerQuiz
--- script.js
+++ script.js
@@ -47,3 +47,112 @@ function startGame() {
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
 }
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+  }
+  
+  function showQuestion(question) { // show question code
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
+  
+  
+  
+  function selectAnswer(e) { // select answer code
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    if (correct === 'true' ){
+      numbersOfQuestionsRight = numbersOfQuestionsRight + 1;
+    }
+    
+    numberOfQuestionsAnswered = numberOfQuestionsAnswered + 1;
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+      quizResults.innerText = 'Questions: '+ numberOfQuestionsAnswered + ' Correct: '+ numbersOfQuestionsRight;
+    }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+  
+  
+  function resetState() {
+    // var finalScoreEl = document.getElementById('final-score')
+    // finalScoreEl.textContent = time
+  
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+  
+  const questions = [ // list of questions 
+    {
+      question: 'What was the first Marvel movie?',
+      answers: [
+        { text: 'Harry Potter', correct: false },
+        { text: 'End Game', correct: false },
+        { text: 'Thor', correct: false },
+        { text: 'Iron Man', correct: true },
+  
+      ]
+    },
+    {
+      question: 'What was the first Marvel movie to earn $1 billion?',
+      answers: [
+        { text: 'The Avengers', correct: true },
+        { text: 'Avatar.', correct: false },
+        { text: 'Doctor Strange.', correct: false },
+        { text: 'Iron Man.', correct: false },
+      ]
+    },
+    {
+      question: 'What are the Name of Tony Starks Parents?',
+      answers: [
+        { text: 'Howard and Maria Stark', correct: true },
+        { text: 'Jack and Jill Stark', correct: false },
+        { text: 'Bruce and Wanda Stark', correct: false },
+        { text: 'Roger and Nancy Stark', correct: false },
+  
+      ]
+    },
+    {
+      question: 'In Avenger Endgame, who kills Thanos in the first half of the movie?',
+      answers: [
+        { text: 'Captain Marvel', correct: false },
+        { text: 'Thor', correct: true },
+        { text: 'Goku', correct: false },
+        { text: 'He tripped', correct: false },
+      ]
+    }
+  ]
